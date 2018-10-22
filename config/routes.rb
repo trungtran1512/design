78e74086bdb1b8e031c2893fc436308b87be32b5
Vo Van Tri <vo.van.tri@inte.co.jp>
2018-10-22 18:42:50 +0700

    home page with custom navbar and images

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'say/hello'
+  get 'say/goodbye'
+	root to: "say#hello"
 end

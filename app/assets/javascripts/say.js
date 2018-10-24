2a0b1e1405a9b6ae169f97ad06f6ed7361b5a0f6
admin <trungpro151224@gmail.com>
2018-10-25 02:35:03 +0700

    modify page menu

--- app/assets/javascripts/say.js
+++ app/assets/javascripts/say.js
@@ -20,13 +20,8 @@ function scrollFunction() {
 }
 
 
-// var $grid = $('.grid').masonry({
-//   itemSelector: '.grid-item',
-//   percentPosition: true,
-//   columnWidth: '.grid-sizer'
-// });
-// // layout Masonry after each image loads
-// $grid.imagesLoaded().progress( function() {
-//   $grid.masonry();
-// });  
-
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});

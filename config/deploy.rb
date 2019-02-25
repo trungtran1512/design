03d3c5136130f8a86571f9eef09c4b4b3dc78363
Tran Huu Trung <trungpro151224@gmail.com>
2019-02-26 01:41:51 +0700

    develop: fix precompile scss with url

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.11.0"
 set :application, "design"
 set :repo_url, "git@github.com:trungpro152224/design.git"
 set :branch, :develop
-set :deploy_to, '/home/deploy/apps/design'
+set :deploy_to, '/deploy/apps/design'
 set :pty, true
 set :linked_files, %w(config/database.yml config/application.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
@@ -31,11 +31,6 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
-
 end

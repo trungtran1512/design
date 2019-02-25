c783b8c3a16e45dc4959385b401c3dd5462b8105
Tran Huu Trung <trungpro151224@gmail.com>
2019-02-26 00:28:39 +0700

    develop: config for deploy server

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,13 @@ lock "3.11.0"
 set :application, "design"
 set :repo_url, "git@github.com:trungpro152224/design.git"
 set :branch, :develop
-set :deploy_to, '/deploy/apps/design'
+set :deploy_to, '/home/deploy/apps/design'
 set :pty, true
 set :linked_files, %w(config/database.yml config/application.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
 set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.5.3'
 
 set :puma_rackup, -> {File.join(current_path, "config.ru")}
 set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
 set :stage, :production
 set :rails_env, :production
-set :deploy_to, "/deploy/apps/design"
-set :branch, :develop
 server "54.202.119.0", user: "webdesign", roles: %w(web app db)

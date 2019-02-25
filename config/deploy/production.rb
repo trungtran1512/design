92f5fab1aefea487fd1efa11cd6ee15de4c60304
Tran Huu Trung <trungpro151224@gmail.com>
2019-02-25 20:46:56 +0700

    develop: update code for upload avatar to s3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, :production
 set :deploy_to, "/deploy/apps/design"
 set :branch, :develop
-server "54.244.57.100", user: "webdesign", roles: %w(web app db)
+server "54.202.119.0", user: "webdesign", roles: %w(web app db)
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,12 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAIMWY3T4I2A3GCJ6Q',
-      aws_secret_access_key: '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',
-      region: 'us-east-1'
+      aws_access_key_id: 'AKIAJQQ2SFVLAOUNMNEA',
+      aws_secret_access_key: '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',
+      region: 'us-west-2'
     }
     config.storage = :fog
-    config.fog_directory = 'design-s3-image'
+    config.fog_directory = 'design-image'
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   else
     config.storage = :file
